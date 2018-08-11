ea48569638979ccddd5864b76ff9dcc7fdb76628
Ryan Bas <krados@gmail.com>
2018-08-11 15:39:51 -0400

    some cleanup before pushing

--- red.py
+++ red.py
@@ -7,52 +7,74 @@ import collections
 import logging
 import sys
 import codecs
+import pathlib
 
-user_agent = [('User-Agent', 'python:com.github.ryanbas.messin.around:v0.0.1 (by /u/ryanbas)')]
+user_agent = [('User-Agent', 'python:com.github.ryanbas.red:v0.0.1 (by /u/ryanbas)')]
 
-def saveToFile(filename, idx, jsonStr):
-  f = open(filename + str(idx) + '.json', 'w')
-  f.write(jsonStr)
+def save_to_file(filename, json_str):
+  pathlib.Path('cached').mkdir(parents = True, exist_ok = True) 
+  file_path = 'cached/{}.json'.format(filename)
+  with open(file_path, 'w') as f:
+    f.write(json_str)
 
-def getPostsFromUrl(subreddit, after = ''):
+def fetch_newest_posts_from_subreddit(subreddit, after = ''):
   opener = urllib.request.build_opener()
   opener.addheaders = user_agent
   post_url = 'https://www.reddit.com/r/{}/new.json?limit=100'
 
   return decode_http_response(opener.open(post_url.format(subreddit) + '&after=' + after))
 
-def getUserInfo(username):
-  opener = urllib.request.build_opener()
-  opener.addheaders = user_agent
-  
-  return decode_http_response(opener.open(user_url.format(username)))
-
 def decode_http_response(response):
   logging.info(response.info())
   return codecs.getreader('UTF-8')(response)
 
-def getPostsFromFile(filename, idx = ''):
-  f = open(filename + str(idx) + '.json')
-  return f.read()
+def get_posts_from_file(filename):
+  file_path = 'cached/{}.json'.format(filename)
+  with open(file_path) as f:
+    return f.read()
 
 def setup_logging(args):
   log_level = getattr(logging, args.log_level, None)
   logging.basicConfig(level = log_level)
 
 parser = argparse.ArgumentParser(description = "Read stuff from reddit")
-parser.add_argument('subreddit', metavar = 'S', help = 'the subreddit to get new posts from')
-parser.add_argument('--log', dest = 'log_level', default = 'WARN', required = False, type = str.upper, choices = ['DEBUG', 'INFO', 'WARN', 'ERROR'])
+parser.add_argument('subreddit', metavar = 'subreddit', help = 'the subreddit to get new posts from')
+parser.add_argument('--log', dest = 'log_level', default = 'ERROR', type = str.upper, choices = ['DEBUG', 'INFO', 'WARN', 'ERROR'], help = 'set the logging level')
+parser.add_argument('--cache', dest = 'cache_response', action = 'store_true', default = False, help = 'store the response from reddit in a file for later use')
+parser.add_argument('--use-cache', dest = 'use_cache', action = 'store_true', default = False, help = 'use a previously stored response instead of retrieving posts from reddit')
 args = parser.parse_args(sys.argv[1:])
 setup_logging(args)
 
 subreddit = args.subreddit
-user_url = 'https://www.reddit.com/u/{}/about.json'
 
-posts_stream = getPostsFromUrl(subreddit)
-data = json.load(posts_stream)
+if args.use_cache:
+  try:
+    json_str = get_posts_from_file(subreddit)
+    print('Using cached posts from ./cached/{}.json'.format(subreddit))
+  except FileNotFoundError:
+    print('Cached file', './cached/{}.json'.format(subreddit), 'does not exist')
+    print('Use --cache parameter instead to retrieve posts from', subreddit)
+else:
+  print('Fetching newest 100 posts from', subreddit, 'subreddit'.format(subreddit))
+  posts_stream = fetch_newest_posts_from_subreddit(subreddit)
+  json_str = posts_stream.read()
+  if args.cache_response:
+    print('Saving posts to ./cached/{}.json'.format(subreddit))
+    save_to_file(subreddit, json_str)
 
-data_children = data['data']['children']
-authors = collections.Counter([postdata['data']['author'] for postdata in data_children])
-print('Total posts: ' + str(len(data_children)))
-for author in authors.most_common(10):
-  print(str(author[0]) + ": " + str(author[1]))
+try:
+  data = json.loads(json_str)
+
+  data_children = data['data']['children']
+  authors = collections.Counter([postdata['data']['author'] for postdata in data_children])
+  print('Total posts: ' + str(len(data_children)))
+
+  header = 'Users with most submissions in {}'.format(subreddit)
+  print()
+  print(header)
+  print(''.join(['-' for x in header]))
+
+  for author in authors.most_common(10):
+    print(str(author[0]) + ": " + str(author[1]))
+except Exception as e:
+  logging.warn(e)
